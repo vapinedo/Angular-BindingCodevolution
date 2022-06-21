c6f1a211169e9eaff67edf2b4a08191309a5239d
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-21 17:16:19 -0500

    [add] Angular Tutorial - 7 - Class Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>Welcome {{name}}</h2>
-    <input id="test" type="text" value="Valp">
-    <!-- <input bind-disabled="isDisabled" type="text" value="Valp"> -->
-    <input [disabled]="isDisabled" type="text" value="Valp">
+    <h2 class="text-success">Welcome {{name}}</h2>
+    <h2 [class]="successClass">Tony Kroos</h2>
+    <h2 class="text-special" [class]="successClass">Karin Benzema</h2>
+    <h2 [class.text-danger]="hasError">Casemiro</h2>
+    <h2 [ngClass]="messageClasses">Vinicius Jr</h2>
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color: green;
+    }
+    .text-danger {
+      color: red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
-  public name = "Valp";
-  public myId = "testId";
-  public isDisabled = true;
-  public siteUrl = window.location.href;
+  hasError: boolean = true;
+  isSpecial: boolean = true;
+  messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  };
+  name: string = "To Real Madrid";
+  successClass: string = "text-success";
 
   constructor() { }
 
