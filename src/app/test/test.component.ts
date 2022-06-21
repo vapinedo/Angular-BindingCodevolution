0fa9f0e5b023473e5541231a2b700c97563951e6
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-21 18:44:58 -0500

    [add] 9 - Event Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,22 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2 [style.color]="hasError ? 'red': 'blue'">Welcome {{name}}</h2>
-    <h2 [style.color]="highLightColor">Santiabo Bernabeu</h2>
-    <h2 [ngStyle]="titleStyles">Luis Figo</h2>
+    <h2>Welcome {{name}}</h2>
+    <h2>{{greeting}}</h2>
+    <button (click)="greet($event)">Greet</button>
+    <button (click)="greeting='Welcome to Home'">Greet</button>
   `,
   styles: []
 })
 export class TestComponent {
 
-  hasError: boolean = false;
   name: string = "Real Madrid";
-  highLightColor: string = "orange";
-  titleStyles = {
-    color: "blue",
-    fontStyle: "italic"
-  };
+  greeting: string = "";
+
+  greet(event: MouseEvent) {
+    console.log(event)
+    this.greeting = event.type;
+  }
 
   constructor() { }
-
 }
