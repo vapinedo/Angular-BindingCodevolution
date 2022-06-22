ada8d15025fef26e76beb5b8723b598a2bc08e74
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-21 20:00:41 -0500

    [add] 10 - Template Reference Variables

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,21 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>Welcome {{name}}</h2>
-    <h2>{{greeting}}</h2>
-    <button (click)="greet($event)">Greet</button>
-    <button (click)="greeting='Welcome to Home'">Greet</button>
+    <input #myInput type="text" />
+    <button (click)="logMessage(myInput)">Log</button>
   `,
   styles: []
 })
 export class TestComponent {
 
   name: string = "Real Madrid";
-  greeting: string = "";
-
-  greet(event: MouseEvent) {
-    console.log(event)
-    this.greeting = event.type;
-  }
 
   constructor() { }
+
+  logMessage(value: any) {
+    
+  }
 }
