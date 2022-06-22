9042d31ed9bc9cce7d3584c1138ac0b8ea39397b
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-22 10:01:01 -0500

    [add] 1 - Two Way Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>Welcome {{name}}</h2>
-    <input #myInput type="text" />
-    <button (click)="logMessage(myInput)">Log</button>
+    <input [(ngModel)]="name" type="text">
+    {{name}}
   `,
   styles: []
 })
 export class TestComponent {
 
-  name: string = "Real Madrid";
+  name: string = "";
 
   constructor() { }
-
-  logMessage(value: any) {
-    
-  }
 }
